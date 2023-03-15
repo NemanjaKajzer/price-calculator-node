ff44e984c369f15dcaa394f08268d23cb8d52534
Nemanja Kajzer <n.kajzer@vega.local>
2023-03-15 17:51:17 +0100

    add tax constructor, add tax test

--- features/tax.test.ts
+++ features/tax.test.ts
@@ -1,6 +1,8 @@
 import { Money } from "../money/money";
+import { Percentage } from "../percentage/percentage";
 import { PriceCalculator } from "../price-calculator/price-calculator";
 import { Product } from "../product/product";
+import { Tax } from "../tax/tax";
 
 test('When only price specified, default tax should be applied', () => {
   var product: Product = {
@@ -13,4 +15,21 @@ test('When only price specified, default tax should be applied', () => {
   var report = calculator.calculate(product);
 
   expect(report.total.toString()).toBe("$24.30");
+});
+
+test('When price and tax specified, custom tax should be applied', () => {
+  var product: Product = {
+    name: "The Little Prince",
+    upc: "12345",
+    price: new Money(20.25)
+  };
+
+  var tax = new Tax(new Percentage(0.21));
+
+  var calculator = new PriceCalculator()
+    .withTax(tax);
+
+  var report = calculator.calculate(product);
+
+  expect(report.total.toString()).toBe("$24.50");
 });
--- tax/tax.ts
+++ tax/tax.ts
@@ -5,6 +5,10 @@ import { ITax } from "./i-tax";
 export class Tax implements ITax {
     percentage: Percentage;
 
+    constructor(percentage: Percentage) {
+        this.percentage = percentage;
+    }
+
     applyTax(price: Money): Money {
         return price.multiply(this.percentage);
     }
